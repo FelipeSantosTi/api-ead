7930d25fb5a468881a5a5bfd305da179190153d8
Felipe Santos <felipessantos.ti@gmail.com>
2022-01-13 14:00:40 -0300

    show course by id

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+
     protected $fillable = [
         'name',
         'email',
